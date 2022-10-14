be22a869061b193bce5c1d0452d2b3cf6b26ba24
qodize <vladislav.gusev.dev@gmail.com>
2022-10-14 21:58:02 +0300

    orders pg, types edit

---
+++ common/db_manager.py
@@ -0,0 +1,47 @@
+import psycopg2
+from .types import *
+import datetime as dt
+from typing import List
+
+pg_database = 'hakaton'
+pg_username = 'postgres'
+pg_password = 'postgres'
+pg_host = '127.0.0.1'
+
+
+def postgres_wrapper(func):
+    def wrap(*args, **kwargs):
+        with psycopg2.connect(dbname=pg_database,
+                              user=pg_username,
+                              password=pg_password,
+                              host=pg_host) as conn:
+            with conn.cursor() as cursor:
+                return func(cursor, *args, **kwargs)
+
+    return wrap
+
+
+class PG_Orders:
+    @staticmethod
+    @postgres_wrapper
+    def insert(cursor, order: Order):
+        cursor.execute(f"""INSERT INTO orders
+         VALUES (DEFAULT,
+                 {order.user.id},
+                 {order.driver.id},
+                 '{order.transport.type}',
+                 '{order.start}',
+                 '{order.end}',
+                 {order.latitude},
+                 {order.longitude},
+                 '{order.status}')
+         RETURNING id""")
+        order_id = cursor.fetchall()[0][0]
+        return order_id
+
+    @staticmethod
+    @postgres_wrapper
+    def get(cursor, order_id: int):
+        cursor.execute(f"""SELECT * FROM orders WHERE id = {order_id}""")
+        order = Order(*cursor.fetchall()[0])
+        return order
--- common/types.py
+++ common/types.py
@@ -5,11 +5,13 @@ from typing import Tuple, Optional
 
 @dataclasses.dataclass
 class User:
+    id: int
     phone: str
 
 
 @dataclasses.dataclass
 class Driver:
+    id: int
     phone: str
 
 
@@ -20,10 +22,19 @@ class Transport:
 
 @dataclasses.dataclass
 class Order:
+    id: Optional[int]
     user: User
     driver: Optional[Driver]
     transport: Transport
     start: dt.datetime
     end: dt.datetime
-    geodata: Tuple[float]
+    latitude: float
+    longitude: float
+    status: str
 
+    @classmethod
+    def from_json(cls, data):
+        user = User(**data.pop('user'))
+        driver = Driver(**data.pop('driver')) if data.get('driver') else None
+        transport = Transport(**data.pop('transport'))
+        return cls(id=data.pop('id', None), user=user, driver=driver, transport=transport, **data)

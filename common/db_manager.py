ca8a3544352b8e96149eff4d1c1469248408ca8f
qodize <vladislav.gusev.dev@gmail.com>
2022-10-14 22:41:28 +0300

    orders pg, types edit

--- common/db_manager.py
+++ common/db_manager.py
@@ -28,7 +28,7 @@ class PG_Orders:
         cursor.execute(f"""INSERT INTO orders
          VALUES (DEFAULT,
                  {order.user.id},
-                 {order.driver.id if order.driver else None},
+                 {order.driver.id if order.driver else "NULL"},
                  '{order.transport.type}',
                  '{order.start}',
                  '{order.end}',
